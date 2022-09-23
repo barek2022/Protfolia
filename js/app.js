9680d1e827cd14907308d64bd1965833413cf3e1
Barek <112208024+barek2022@users.noreply.github.com>
2022-09-23 07:00:28 -0700

    Protfolio

---
+++ js/app.js
@@ -0,0 +1,48 @@
+//navbar
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//navbar
+
+// portfolio filter JS
+
+let sortBtn = document.querySelector(".filter-menu").children;
+let sortItem = document.querySelector(".filter-item").children;
+
+for (let i = 0; i < sortBtn.length; i++) {
+  sortBtn[i].addEventListener("click", function () {
+    for (let j = 0; j < sortBtn.length; j++) {
+      sortBtn[j].classList.remove("current");
+    }
+
+    this.classList.add("current");
+
+    let targetData = this.getAttribute("data-target");
+
+    for (let k = 0; k < sortItem.length; k++) {
+      sortItem[k].classList.remove("active");
+      sortItem[k].classList.add("delete");
+
+      if (
+        sortItem[k].getAttribute("data-item") == targetData ||
+        targetData == "all"
+      ) {
+        sortItem[k].classList.remove("delete");
+        sortItem[k].classList.add("active");
+      }
+    }
+  });
+}
